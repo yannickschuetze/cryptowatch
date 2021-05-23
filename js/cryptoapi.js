0d1fd46f895ab910fea65c3e7806911a71ca5c7f
yannickschuetze <yannick.schuetze@gmx.de>
2021-05-23 11:33:49 +0200

    api changes

--- js/cryptoapi.js
+++ js/cryptoapi.js
@@ -1,47 +1,56 @@
-const api_url = 'https://api.coinlore.net/api/ticker/?id='
-const coins = {
-    btc: 90,
-    eth: 80,
-    ada: 257,
-    doge: 2
+const api_url = 'https://api.coingecko.com/api/v3/'
+const currency = 'usd'
+
+function getTopCoins() {
+    fetch(api_url + 'coins/markets?vs_currency=' + currency + '&order=market_cap_desc&per_page=4&page=1&sparkline=false&price_change_percentage=24h')
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+        for(var i = 0; i < data.length; i++) {
+            console.log(data[i].name)
+            $('#crypto-top-' + i + ' .lds-ring').css('display', 'none')
+            $('.dashboard__crypto-card--price').fadeIn('slow')
+            $('#crypto-top-' + i + ' .dashboard__crypto-card--title').text(data[i].name + ' (' + data[i].symbol + ')')
+            $('#crypto-top-' + i + ' .dashboard__crypto-card--price').text(data[i].current_price + '$')
+
+        }
+    })
+    .catch((err) => console.error("Rejected", err))
+    // apiLoaded()
 }
 
-function getCoins() {
-    for (const [key, value] of Object.entries(coins)) {
-        const priceCoin = document.getElementById('price' + key)
-
-        fetch(api_url + value)
-        .then((res) => res.json())
-        .then((data) => {
-            priceCoin.innerHTML = data["0"].price_usd + ' USD'
-
-            if(data["0"].percent_change_24h  <= 0) {
-                $('.dashboard__crypto-card--status').text('↘')
-                $('.dashboard__crypto-card--status').css('color', 'red')
-            } else {
-                $('.dashboard__crypto-card--status').text('↗')
-                $('.dashboard__crypto-card--status').css('color', 'green')
-            }
-
-            console.log(data)
-
-        })
-        .catch((err) => console.error("Rejected", err))
-        apiLoaded()
-    }
+function getTrendingCoins() {
+    fetch(api_url + 'search/trending')
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+        for(var i = 0; i < 4; i++) {
+            $('#crypto-trending-' + i + ' .lds-ring').css('display', 'none')
+            $('#crypto-trending-' + i + ' .dashboard__crypto-card--title').text(data.coins[i].item.name + ' (' + data.coins[i].item.symbol + ')')
+        }
+    })
+    .catch((err) => console.error("Rejected", err))
+    //apiLoaded()
 }
 
-function apiLoaded() {
-    $('.lds-ring').css('display', 'none')
-    
-    $('.dashboard__crypto-card--price').fadeIn('slow')
-
-
+function refreshPrices() {
+    fetch(api_url + 'coins/markets?vs_currency=' + currency + '&order=market_cap_desc&per_page=4&page=1&sparkline=false&price_change_percentage=24h')
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+        for(var i = 0; i < data.length; i++) {
+            console.log(data[i].name)
+            $('#crypto-top-' + i + ' .dashboard__crypto-card--price').text(data[i].current_price + ' ' + currency)
+        }
+    })
+    .catch((err) => console.error("Rejected", err))
 }
 
+getTopCoins()
+getTrendingCoins()
 
 var intervalId = window.setInterval(function(){
-    getCoins()
-  }, 3000)
+    refreshPrices()
+  }, 10000)
 
 
